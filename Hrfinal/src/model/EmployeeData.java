01a971f553ea6bbd4e2af9e203b227da818fe6d8
shubhamsable <shubhamsable@localhost>
2022-10-02 13:23:04 -0400

    First

---
+++ Hrfinal/src/model/EmployeeData.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author shubhamsable
+ */
+public class EmployeeData {
+    private String name;
+    private int id;
+    private  int age;
+    private String gender; 
+    private int sDate;
+    private int level;
+    private String team;
+    private String position;
+    private int cNUm;
+    private String cEmail;
+    private String photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getsDate() {
+        return sDate;
+    }
+
+    public void setsDate(int sDate) {
+        this.sDate = sDate;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getcNUm() {
+        return cNUm;
+    }
+
+    public void setcNUm(int cNUm) {
+        this.cNUm = cNUm;
+    }
+
+    public String getcEmail() {
+        return cEmail;
+    }
+
+    public void setcEmail(String cEmail) {
+        this.cEmail = cEmail;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+    
+}
