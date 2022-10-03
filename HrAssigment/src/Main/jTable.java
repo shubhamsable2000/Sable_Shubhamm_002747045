5429a7a2d4a3bf87679b39f9beb87de02a9f7e14
shubhamsable <shubhamsable@localhost>
2022-10-02 23:27:36 -0400

    final

---
+++ HrAssigment/src/Main/jTable.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author shubhamsable
+ */
+class jTable {
+
+    static int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static int getSelectedRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
